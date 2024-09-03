97dc4ff086ddc442faf25f40cad5f02985862862
Edgar Romero Cuc Chocoj <118028625+edgarromerodev@users.noreply.github.com>
2024-09-03 06:00:36 -0600

    Update script.js
    actualice el codigo css

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
- document.getElementById("botoContar").addEventListener("click", contar);
- document.getElementById("botonLimpiar").addEventListener("click", limpiar);
+document.addEventListener("DOMContentLoaded", () =>{
+    const botonContar = document.getElementById("botonContar")
+    botonContar.addEventListener("click", contar);
 
-function contar () {
-    const recortarTexto = document.getElementById("textInput").value.trim();
-    const palabrasContada  = recortarTexto.split(/\s+/).filter(elemento => elemento !== "").length;
-    document.getElementById("contadorPalabra").textContent = `Número de palabras: ${palabrasContada}`
-    const caracteresContadas = recortarTexto.split("").filter(letra => letra !== " " && letra !== "\n" && letra !== "\t").length
-    document.getElementById("contadorCaracteres").textContent = `Número de caracteres ${caracteresContadas}`
-}
+    const botonLimpiar = document.getElementById("botonLimpiar")
+    botonLimpiar.addEventListener("click", limpiar)
 
-function limpiar () {
-    document.getElementById("textInput").value = "";
-    document.getElementById("contadorPalabra").textContent = "Número de palabras: 0";
-    document.getElementById("contadorCaracteres").textContent = "Número de caracteres: 0";
-}
+    const mostrarCantidadDepalabras = document.getElementById("contadorPalabra")
+    const mostraCantidadDeCaracteres = document.getElementById("contadorCaracteres")
+    function contar () {
+         const datosRecibidos = document.getElementById("textInput").value;
+         const textoRecortado = datosRecibidos.trim();
+         const arreglodePalabras = textoRecortado.split(/\s+/);
+         const cantidadDePalabras = arreglodePalabras.length;
+         mostrarCantidadDepalabras.textContent = `Número de palabras: ${cantidadDePalabras}` 
 
+        
+         const arregloDeCaracteres = textoRecortado.split("")
+         const eliminarElementosVacios = arregloDeCaracteres.filter(caracter =>
+         caracter !== " " && caracter !== "\n" && caracter !== "\t"
+         )
+         const cantidadDeCaracteres = eliminarElementosVacios.length;
+
+         mostraCantidadDeCaracteres.textContent =  `Número de caracteres: ${cantidadDeCaracteres}`
+    }
+
+
+    function limpiar () {
+        document.getElementById("textInput").value = "";
+        mostrarCantidadDepalabras.textContent = "Número de palabras: 0";
+        mostraCantidadDeCaracteres.textContent = "Número de caracteres: 0"
+    }
+
+
+})
